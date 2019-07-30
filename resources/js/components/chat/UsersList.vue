5bae735efa1c66203be54bffdd13eb80097ec369
mlazuardy <lazuardymichael@gmail.com>
2019-07-30 14:37:45 +0700

    Chat message and usersl ist componetn

---
+++ resources/js/components/chat/UsersList.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="inbox_chat">
+        <div class="chat_list active_chat" v-for="user in users" :key="user.id" @click="addTarget(user)">
+            <div class="chat_people">
+                <div class="chat_ib">
+                    <h5>{{user.name}}</h5>
+                    <p>Tes message</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import { mapState } from 'vuex';
+
+export default {
+    methods: {
+        addTarget(user) {
+            this.$store.commit('messages/addTarget',user);
+        }
+    },
+    mounted() {
+        this.$store.dispatch('users/index');
+    },
+    computed: {
+        ...mapState({
+            users: state => state.users.all
+        })
+    }
+}
+</script>
